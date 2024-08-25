e0739e6d1cb672a9d2df0657ab0d72ef28b04e0a
MuhammetYazici <yzcmm61@gmail.com>
2024-08-25 16:36:46 +0300

    değişiklik

--- src/Muhammet.java
+++ src/Muhammet.java
@@ -2,5 +2,6 @@ public class Muhammet {
     public static void main(String[] args) {
         System.out.println("Merhaba");
         System.out.println("betül değişiklik_1");
+        System.out.println("muhammet değişiklik");
     }
 }
